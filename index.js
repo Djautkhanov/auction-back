d0e0a411d5c62bb36ebf790967a288551d922abc
Yakumov <dgahar.yakumov@gmail.com>
2023-04-11 12:24:59 +0300

    Внес изменения для аукциона

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json())
 app.use(fileUpload())
 app.use(cors());
 
-app.use('/uploads', express.static(__dirname + '/uploads'));
+app.use('/uploads', express.static(__dirname + '/uploads')); 
 app.use(require("./routes/auction.route"))
 app.use(require("./routes/category.route"))
 app.use(require("./routes/item.route"))
